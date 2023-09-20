528884093408916ba78614c8510575cb3a8dc54c
Felipe <riopipe8@gmail.com>
2023-09-20 17:01:06 -0300

    Agregado clase para iniciar la aplicación

---
+++ src/InicioApp.java
@@ -0,0 +1,20 @@
+import java.awt.EventQueue;
+
+import com.alura.hotel.views.MenuPrincipal;
+
+public class InicioApp {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipal frame = new MenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
